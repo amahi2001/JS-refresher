4cabab0b0e69d5bec7cd62c07366013d0d337c5b
Abrar <amahi2001@gmail.com>
2022-09-26 16:34:15 -0400

    arrow functions

---
+++ arrow_functions.js
@@ -0,0 +1,17 @@
+
+//normal function
+function normal(name){
+    console.log(name);
+}
+normal('normal function');
+
+//arrow 
+const arrow = (name) => {
+    console.log(name)
+}
+arrow('Arrow function')
+
+
+//omitting curly braces
+const multiplyBy2 = (number) => number*2
+console.log(multiplyBy2(12));
